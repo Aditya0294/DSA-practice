87431201bd27b1d688ddfd367da44d0964710345
Aditya0294 <adisharma0294@gmail.com>
2021-08-16 21:04:46 +0530

    stack

---
+++ stack/nearest_smallest_on_left.cpp
@@ -0,0 +1,28 @@
+vector<int> leftSmaller(int n, int arr[]){
+       vector<int> res;
+       stack<int> s;
+       for(int i=0;i<n;i++)
+       {
+           if(s.size()==0)
+             res.push_back(-1);
+           else if(s.size()>0 && s.top()<arr[i])
+           {
+               res.push_back(s.top());
+           }
+           else if(s.size()>0 && s.top()>=arr[i])
+           {
+               while(s.size()>0 && s.top()>=arr[i])
+               {
+                   s.pop();
+               }
+               if(s.size()==0)
+                  res.push_back(-1);
+                else
+                {
+                    res.push_back(s.top());
+                }
+           }
+           s.push(arr[i]);
+       }
+       return res;
+    }
